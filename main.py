019848c5f113afca7dafe79135d8bf59d1260ed2
ink2421665373 <97945317+ink2421665373@users.noreply.github.com>
2026-05-02 21:14:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-wfrom astrbot.api.event import filter, AstrMessageEvent
+from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star
 from astrbot.api import logger
 from astrbot.api import AstrBotConfig
